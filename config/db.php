8d3661532aaa069772d1b77ade567e071ac9dd84
YokiToki <libastral.so@yandex.ru>
2017-10-04 10:34:55 +0700

    Первый коммит

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=jkh',
+    'username' => 'root',
+    'password' => 'toor',
+    'charset' => 'utf8',
+];
